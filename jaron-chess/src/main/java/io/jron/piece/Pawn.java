4ea2fbc6438a69c91524cd7cc045f681324d7448
Jaron-Wilson <78304954+Jaron-Wilson@users.noreply.github.com>
2021-11-07 14:16:38 -0500

    Fixed Pawns

---
+++ jaron-chess/src/main/java/io/jron/piece/Pawn.java
@@ -0,0 +1,83 @@
+package io.jron.piece;
+
+import io.jron.Board;
+import io.jron.Coordanate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pawn extends Piece {
+
+    public Pawn(Color color) {
+        super(color);
+    }
+
+    @Override
+    public List<Coordanate> canMoveTo(Board board, Coordanate current) {
+        List<Coordanate> moves = new ArrayList<>(4);
+
+        int x = current.getX();
+
+
+        if (getColor() == Color.BLACK) {
+            int maxY = current.getY()  + (current.getY() == 1 ? 2 : 1 );
+            for (int y = current.getY() + 1; y <= maxY; y++) {
+                Piece spot = board.getPiece(x, y);
+                if (spot == null) {
+                    moves.add(new Coordanate(x, y));
+                } else {
+                    break;
+                }
+            }
+
+            if( x>0) {
+//                 Checks [ ][P][ ]
+//                        [x][ ][ ]
+                Piece capture = board.getPiece(x - 1, current.getY() + 1);
+                if (capture != null && capture.getColor() == Color.WHITE) {
+                    moves.add(new Coordanate(x - 1, current.getY() + 1));
+                }
+            }
+
+            if( x < board.getWidth()-1) {
+//                 Checks [ ][P][ ]
+//                        [ ][ ][x]
+                Piece capture = board.getPiece(x + 1, current.getY() + 1);
+                if (capture != null && capture.getColor() == Color.WHITE) {
+                    moves.add(new Coordanate(x + 1, current.getY() + 1));
+                }
+            }
+        }
+        if (getColor() == Color.WHITE) {
+            int maxY = current.getY() - (current.getY() == 6 ? 2 : 1);
+            for (int y = current.getY() - 1; y >= maxY; y--) {
+                Piece spot = board.getPiece(x, y);
+                if (spot == null) {
+                    moves.add(new Coordanate(x, y));
+                } else {
+                    break;
+                }
+            }
+            if( x > 0) {
+//                 Checks [x][ ][ ]
+//                        [ ][P][ ]
+                Piece capture = board.getPiece(x - 1, current.getY() - 1);
+                if (capture != null && capture.getColor() == Color.BLACK) {
+                    moves.add(new Coordanate(x - 1, current.getY() - 1));
+                }
+            }
+
+            if( x < board.getWidth() - 1) {
+//                 Checks [ ][ ][x]
+//                        [ ][P][ ]
+                    Piece capture = board.getPiece(x + 1, current.getY() - 1);
+                    if (capture != null && capture.getColor() == Color.BLACK) {
+                        moves.add(new Coordanate(x + 1, current.getY() - 1));
+                }
+            }
+        }
+
+
+        return moves;
+    }
+}
---
+++ jaron-chess/src/main/java/io/jron/piece/Rook.java
@@ -0,0 +1,43 @@
+package io.jron.piece;
+
+import io.jron.Board;
+import io.jron.Coordanate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rook extends Piece {
+
+    public Rook(Color color) {
+        super(color);
+    }
+
+    @Override
+    public List<Coordanate> canMoveTo(Board board, Coordanate current) {
+        List<Coordanate> moves = new ArrayList<>(14);
+
+        for (int y = current.getY(), x = current.getX() - 1; x >= 0; x--) {
+           if (!addIfEmptyAndMoreThanThat(moves, board, x, y)) {
+               break;
+           }
+        }
+        for (int y = current.getY(), x = current.getX() + 1; x < board.getWidth(); x++) {
+            if (!addIfEmptyAndMoreThanThat(moves, board, x, y)) {
+                break;
+            }
+        }
+
+        for (int x = current.getX(), y = current.getY() + 1; y < board.getWidth(); y++) {
+            if (!addIfEmptyAndMoreThanThat(moves, board, x, y)) {
+                break;
+            }
+        }
+        for (int x = current.getX(), y = current.getY() - 1; y >= 0; y--) {
+            if (!addIfEmptyAndMoreThanThat(moves, board, x, y)) {
+                break;
+            }
+        }
+
+        return moves;
+    }
+}
