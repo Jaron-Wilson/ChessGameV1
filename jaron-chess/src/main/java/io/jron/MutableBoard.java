2741930a98e548df6f602da0bb0a5fcd828abf64
Jaron-Wilson <78304954+Jaron-Wilson@users.noreply.github.com>
2021-11-07 12:35:55 -0500

    Added turns

---
+++ jaron-chess/src/main/java/io/jron/MutableBoard.java
@@ -0,0 +1,31 @@
+package io.jron;
+
+import io.jron.piece.Piece;
+
+public class MutableBoard implements Board {
+
+    private final Piece[][] board = new Piece[8][8];
+
+    @Override
+    public int getWidth() {
+        return 8;
+    }
+
+    @Override
+    public int getHeight() {
+        return 8;
+    }
+
+    @Override
+    public Piece getPiece(int x, int y) {
+        return board[x][y];
+    }
+
+    public Piece setPiece(Coordanate c, Piece piece) {
+        return setPiece(c.getX(), c.getY(), piece);
+    }
+    public Piece setPiece(int x, int y, Piece piece) {
+        board[x][y] = piece;
+        return piece;
+    }
+}
